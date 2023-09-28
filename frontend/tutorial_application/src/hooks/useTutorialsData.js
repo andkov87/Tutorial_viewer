27b9715ebf0b18827e9c03eaef5f5a652faa3ae2
Andras <kovalcsik.andras87@gmail.com>
2023-09-28 15:15:32 +0200

    functioning 'add' + axios-setup

---
+++ frontend/tutorial_application/src/hooks/useTutorialsData.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import axiosInstance from '../AxiosConfig';
+
+
+
+const useTutorialsData = () => {
+  const [tutorialData, setTutorialData] = useState(null);
+
+  const fetchTutorialData = async () => {
+    try {
+      const response = await axiosInstance.get('/api/tutorials');
+      const fetchedTutorialData = await response.data;
+      setTutorialData(fetchedTutorialData);
+      console.log("tutorials are fetched", response.data);
+
+    } catch (error) {
+      console.error("problem with fetching:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTutorialData();
+  }, [])
+
+  return [tutorialData, fetchTutorialData];
+}
+
+export default useTutorialsData;
